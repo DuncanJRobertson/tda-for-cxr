b8838b7fcc4d64146770f1d3ecf2e8d92c740d65
Duncan Robertson <duncanr19@gmail.com>
2019-08-19 18:50:25 +0200

    Refactoring with Black

--- perseus.py
+++ perseus.py
@@ -13,14 +13,14 @@ def persistence_stats(ints):
     """Compute summary statistics from lifetimes of persistence intervals."""
     ints.replace(-1, 255, inplace=True)
     life = ints["Death"] - ints["Birth"]
-    n_life = life/sum(life)
-    midlife = (ints["Birth"] + ints["Death"])/2
+    n_life = life / sum(life)
+    midlife = (ints["Birth"] + ints["Death"]) / 2
     data = pd.concat([n_life.rename("n_life"), midlife.rename("midlife")], axis=1)
     summary = data.describe().iloc[1:7, :]
-    summary.loc['skewness'] = data.skew()
-    summary.loc['kurtosis'] = data.kurtosis()
+    summary.loc["skewness"] = data.skew()
+    summary.loc["kurtosis"] = data.kurtosis()
     stack = summary.stack()
-    stack.loc['entropy'] = -sum(n_life*np.log(n_life))
+    stack.loc["entropy"] = -sum(n_life * np.log(n_life))
     return stack
 
 
@@ -35,16 +35,16 @@ def perseus_summarise(img_filepath, img_filename):
     lungs = lf.get_lungs(img)
     img_dim = np.shape(lungs)
     perseus_list = np.concatenate([np.array([2]), img_dim, lungs.flatten()])
-    with open('temp.txt', 'w') as temp:
-        temp.write('\n'.join(str(n) for n in perseus_list))
+    with open("temp.txt", "w") as temp:
+        temp.write("\n".join(str(n) for n in perseus_list))
         subprocess.check_output("perseus cubtop temp.txt out")
         ints_0 = pd.read_csv("out_0.txt", sep=" ", names=["Birth", "Death"])
         ints_1 = pd.read_csv("out_1.txt", sep=" ", names=["Birth", "Death"])
-        betti = pd.read_csv("out_betti.txt", sep=" ", usecols=[2, 3],
-                            names=["b0", "b1"])
+        betti = pd.read_csv(
+            "out_betti.txt", sep=" ", usecols=[2, 3], names=["b0", "b1"]
+        )
         # Betti numbers are not always listed for small t, so we pad with zeros
-        zeros = pd.DataFrame(np.zeros((256 - len(betti), 2)),
-                             columns=["b0", "b1"])
+        zeros = pd.DataFrame(np.zeros((256 - len(betti), 2)), columns=["b0", "b1"])
         betti = pd.concat([zeros, betti])
         betti.index = range(0, 256)
         stats_0 = persistence_stats(ints_0)
@@ -63,7 +63,7 @@ def perseus_loop(img_path_string):
     Uses tqdm to track progress."""
     img_path = Path(img_path_string)
     out = []
-    n_files = len(fnmatch.filter(os.listdir(img_path), '*.png'))
+    n_files = len(fnmatch.filter(os.listdir(img_path), "*.png"))
     with os.scandir(img_path) as folder:
         for entry in tqdm(folder, total=n_files):
             if entry.name.endswith(".png") and entry.is_file():
